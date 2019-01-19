e842ce89f0585b0900e5f5615a16a3d8e746b3da
davidneneg <davidsoares97@outlook.com>
2019-01-18 22:31:07 -0200

    add function controlFab

--- WorkSpace/Drogaria/src/br/com/drogaria/dao/FabricanteDAO.java
+++ WorkSpace/Drogaria/src/br/com/drogaria/dao/FabricanteDAO.java
@@ -16,57 +16,57 @@ public class FabricanteDAO {
 		sql.append("INSERT INTO fabricante ");
 		sql.append("(descricao) ");
 		sql.append("VALUES (?)");
-		
+
 		Connection con= ConexaoFactory.conectar();
-		
+
 		PreparedStatement stmt= con.prepareStatement(sql.toString());
 		stmt.setString(1, f.getDescricao());
-		
+
 		stmt.executeUpdate();
 	    ConexaoFactory.closeConnection(con, stmt);
 	}
-	
+
 	public void excluir(Fabricante f) throws SQLException{
 		StringBuilder sql= new StringBuilder();
 		sql.append("DELETE FROM fabricante ");
 		sql.append("WHERE codigo=? ");
-		
+
 		Connection con= ConexaoFactory.conectar();
-		
+
 		PreparedStatement stmt= con.prepareStatement(sql.toString());
 		stmt.setLong(1, f.getCodigo());
-		
+
 		stmt.executeUpdate();
 		ConexaoFactory.closeConnection(con, stmt);
 	}
-	
+
 	public void editar(Fabricante f)throws SQLException{
 		StringBuilder sql= new StringBuilder();
 		sql.append("UPDATE fabricante ");
 		sql.append("SET descricao=? ");
 		sql.append("WHERE codigo=? ");
-		
+
 		Connection con= ConexaoFactory.conectar();
-		
+
 		PreparedStatement stmt= con.prepareStatement(sql.toString());
 		stmt.setString(1, f.getDescricao());
 		stmt.setLong(2, f.getCodigo());
-		
+
 		stmt.executeUpdate();
 		ConexaoFactory.closeConnection(con, stmt);
 	}
-	
+
 	public Fabricante buscarPorCodigo(Fabricante f) throws SQLException{
 		StringBuilder sql= new StringBuilder();
 		sql.append("SELECT codigo,descricao FROM fabricante ");
 		sql.append("WHERE codigo=? ");
-		
+
 		Connection con= ConexaoFactory.conectar();
 		PreparedStatement stmt= con.prepareStatement(sql.toString());
 		stmt.setLong(1, f.getCodigo());
-		
+
 		ResultSet rs= stmt.executeQuery();
-		
+
 		Fabricante retorno= null;
 		if(rs.next()){
 			retorno= new Fabricante();
@@ -76,54 +76,62 @@ public class FabricanteDAO {
 		ConexaoFactory.closeConnection(con, stmt, rs);
 		return retorno;
 	}
-	
+
 	public ArrayList<Fabricante> listar() throws SQLException{
 		StringBuilder sql= new StringBuilder();
 		sql.append("SELECT* FROM ");
 		sql.append("fabricante ");
 		sql.append("ORDER BY descricao ASC ");
-		
+
 		Connection con= ConexaoFactory.conectar();
 		PreparedStatement stmt= con.prepareStatement(sql.toString());
 		ResultSet rs= stmt.executeQuery();
-		
+
 		ArrayList<Fabricante> lista= new ArrayList<Fabricante>();
-		
+
 		while(rs.next()){
 			Fabricante f= new Fabricante();
 			f.setCodigo(rs.getLong("codigo"));
 			f.setDescricao(rs.getString("descricao"));
-			
+
 			lista.add(f);
 		}
 		ConexaoFactory.closeConnection(con, stmt, rs);
 		return lista;
 	}
-	
+
 	public ArrayList<Fabricante> buscarPorDescricao(Fabricante f) throws SQLException {
 		StringBuilder sql= new StringBuilder();
 		sql.append("SELECT* FROM ");
 		sql.append("fabricante ");
 		sql.append("WHERE descricao LIKE ? ");
 		sql.append("ORDER BY descricao ASC ");
-		
+
 		Connection con= ConexaoFactory.conectar();
 		PreparedStatement stmt= con.prepareStatement(sql.toString());
 		stmt.setString(1, "%"+ f.getDescricao() +"%");
-		
+
 ResultSet rs= stmt.executeQuery();
-		
+
 		ArrayList<Fabricante> lista= new ArrayList<Fabricante>();
-		
+
 		while(rs.next()){
 			Fabricante fab= new Fabricante();
 			fab.setCodigo(rs.getLong("codigo"));
 			fab.setDescricao(rs.getString("descricao"));
-			
+
 			lista.add(fab);
 		}
 		ConexaoFactory.closeConnection(con, stmt, rs);
 		return lista;
 	}
-	
+
+	public int controlFab(Fabricante fabricante){
+		if(fabricante ==null){
+			return 0;
+		}else {
+			return 1;
+		}
+	}
+
 }
